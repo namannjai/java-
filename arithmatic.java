2bec01dcc3e168b65c1e7e90efe3822011c21dab
namanbhaijaiho-dev <namanjaiswal1119@gmail.com>
2026-03-29 12:42:39 +0530

    update

---
+++ arithmatic.java
@@ -0,0 +1,12 @@
+public class arithmatic{
+    public static void main(String args[]) {
+	int numOne = 10;
+	int numTwo = 5;
+	System.out.println(numOne + numTwo); //Output will be 15
+	System.out.println(numOne - numTwo); //Output will be 5
+	System.out.println(numOne * numTwo); //Output will be 50
+	System.out.println(numOne / numTwo); //Output will be 2
+	System.out.println(numOne % numTwo); //Output will be 0
+}
+
+}
