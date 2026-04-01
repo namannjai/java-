ccc8d8365d4ab667f0e1281f04875d79144ad32c
namanbhaijaiho-dev <namanjaiswal1119@gmail.com>
2026-04-01 09:50:42 +0530

    code

---
+++ scs1.java
@@ -0,0 +1,12 @@
+public class scs1{
+    public static void main(String []args){
+        int num1=11;
+        int num2=10;
+        int sum =num1 +num2;
+        if(num1==num2){
+            System.out.println(sum);
+        }else{
+            System.out.println(2*sum);
+        }
+    }
+}
