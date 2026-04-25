b02cd3a9625fbf523cddbeede56b000fc6980793
namannjai <namanjaiswal1119@gmail.com>
2026-04-25 19:02:16 +0530

    run time polymorphism code

---
+++ DMD.java
@@ -0,0 +1,30 @@
+class A{
+        public void show(){
+            System.out.println("In A Show");
+        }
+}
+class B extends A{
+    public void show(){
+            System.out.println("In B Show");
+        }
+        
+}
+class C extends A{
+public void show(){
+            System.out.println("In C Show");
+        }
+}
+
+public class DMD{
+    public static void main(String[] args) {
+        A obj= new A();
+        obj.show();
+
+        obj = new B();
+        obj.show();
+
+        obj = new C();
+        obj.show();
+
+            }
+}
