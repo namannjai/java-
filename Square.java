27a813159fcefe680d3e05c49204e175e3e6d001
namannjai <namanjaiswal1119@gmail.com>
2026-04-26 11:08:57 +0530

    updated code square pattern

--- Square.java
+++ Square.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Star{
+public class Square{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
