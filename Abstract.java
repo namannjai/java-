2543488c8715dcb4f2993eae26e7d74e12c7a017
namannjai <namanjaiswal1119@gmail.com>
2026-04-30 18:00:16 +0530

    abstract class codes

---
+++ Abstract.java
@@ -0,0 +1,20 @@
+abstract class Car{
+    public abstract void drive();
+    public void playMusic(){
+        System.out.println("punjabi aa gaye oye");
+    }
+
+}
+ class GWagon extends Car{
+    public void drive(){
+        System.out.println("broom broom ...");
+    }
+}
+
+public class Abstract{
+    public static void main(String[] args) {
+        Car obj = new GWagon();
+        obj.playMusic();
+        obj.drive();
+    }
+}
